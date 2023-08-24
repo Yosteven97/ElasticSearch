f6bab222e922e04e0f49179c50b46dcfe1b38eeb
Yosteven <yosteven.yosteven@adira.co.id>
2023-08-24 19:58:24 +0700

    asdf

--- backend/elastic-client.js
+++ backend/elastic-client.js
@@ -2,11 +2,11 @@ const { Client } = require("@elastic/elasticsearch");
 
 const elasticClient = new Client({
   cloud: {
-    id: 'a5753321effe42d582fe5c11e90c54a5:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQzNWMzOWQ5YTk5ODg0ZDM0YjIzZWEwYzkyOWVjMmEzOCRmYjc1OTlmMTFiZDg0ZmI3YjQ1YmI3NGJkNGUwMTliMg==',
+    id: 'ID'
   },
   auth: {
-    username: 'enterprise_search',
-    password: 'html5@970504',
+    username: 'USERNAME',
+    password: 'PASSWORD',
   },
 });
 
